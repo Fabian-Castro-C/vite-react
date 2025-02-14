a75fe44a3a9dc1f25fe7faa7fd99e03322a953d6
Fabian-Castro-C <fabian.castro.c@ug.uchile.cl>
2025-02-13 21:14:00 -0300

    Update 'No' response message with gender-specific wording

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
   };
 
   const handleNo = () => {
-    setRespuesta('¡Oh no! ¿Seguro que no quieres ser mi Valentín?');
+    setRespuesta('¡Oh no! ¿Segura que no quieres ser mi Valentín?');
   };
 
   const handleTitleClick = () => {
